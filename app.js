355635df2617c897b9991d3d42cb85547bd989eb
wemowog737 <wemowog737@ubinert.com>
2024-02-06 23:40:49 +0100

    v0

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.get('/', (req, res) => {
 app.get('/hello', (req, res) => {
     res.send('Hello World!')
 })
-// app.get('/h', (req, res) => {
-//     res.send('Hello World!')
-// })
+app.get('/h', (req, res) => {
+    res.send('Hello World!')
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
