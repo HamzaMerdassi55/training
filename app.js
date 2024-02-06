15a88a3637d71837b6275eaef3f135c14d6d7cd9
Merdassi Hamza <148788487+HamzaMerdassi55@users.noreply.github.com>
2024-02-06 23:47:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Xelcome')
+  res.send('welcome')
 })
 app.get('/hello', (req, res) => {
     res.send('Hello World!')
 })
-app.get('/h', (req, res) => {
-    res.send('Hello World!')
+app.get('/bonjour', (req, res) => {
+    res.send('bonjour')
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
